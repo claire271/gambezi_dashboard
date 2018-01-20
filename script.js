009dd59a375ff4fe5ed0665097653918ec6d10cc
tigerh <tigerrun1998@gmail.com>
2018-01-20 15:07:43 -0500

    Added input and output fields for nodes.

--- script.js
+++ script.js
@@ -45,15 +45,82 @@ function DataNode(gambeziNode, parentDiv) {
 	}.bind(this);
 	let add = document.createElement('button');
 	add.innerHTML = '+';
+
+	// Add handler
 	add.onclick = function(event) {
+		// Create div
 		let div = document.createElement('div');
+		div.classList.add('view_node');
+		div.style.width = grid_size * 3 + 'px';
+		div.style.height = grid_size * 1 + 'px';
+
+		// Create header
+		let header = document.createElement('div');
+		let settings = document.createElement('a');
+		settings.innerHTML = '&#9881;';
+		settings.onclick = function(event) {
+			// Create context menu
+			let menu = document.createElement('div');
+			menu.classList.add('view_menu');
+			menu.style.left = event.clientX - view.offsetLeft;
+			menu.style.top = event.clientY - view.offsetTop;
+
+			// Create buttons
+			let close = document.createElement('a');
+			close.innerHTML = 'Close';
+			close.onclick = function(event) {
+				view.removeChild(menu);
+			};
+			let remove = document.createElement('a');
+			remove.innerHTML = 'Remove';
+			remove.onclick = function(event) {
+				view.removeChild(menu);
+				clear_contents(gambeziNode, div, contents);
+				view.removeChild(div);
+			};
+
+			// Add
+			menu.appendChild(close);
+			menu.appendChild(document.createElement('br'));
+			menu.appendChild(remove);
+
+			// Change data type buttons
+			let data_type = div.getAttribute('data_type');
+			if(data_type != 'input_number') {
+				let button = document.createElement('a');
+				button.innerHTML = 'Input Number';
+				button.onclick = function(event) {
+					view.removeChild(menu);
+					clear_contents(gambeziNode, div, contents);
+					create_input_number(gambeziNode, div, contents);
+				};
+				menu.appendChild(document.createElement('br'));
+				menu.appendChild(button);
+			}
+			if(data_type != 'output_number') {
+				let button = document.createElement('a');
+				button.innerHTML = 'Output Number';
+				button.onclick = function(event) {
+					view.removeChild(menu);
+					clear_contents(gambeziNode, div, contents);
+					create_output_number(gambeziNode, div, contents);
+				};
+				menu.appendChild(document.createElement('br'));
+				menu.appendChild(button);
+			}
+
+			// Add
+			view.appendChild(menu);
+		};
+		header.appendChild(document.createTextNode(gambeziNode.get_string_key().join('/')));
+		header.appendChild(settings);
+		
+		// Create contents
 		let contents = document.createElement('div');
-		let field = document.createElement('input');
-		setInterval(function() {
-			field.value = gambeziNode.get_double();
-		}, 100);
-		contents.appendChild(field);
-		div.appendChild(document.createTextNode(gambeziNode.get_string_key().join('/')));
+		create_output_number(gambeziNode, div, contents);
+
+		// Add 
+		div.appendChild(header);
 		div.appendChild(contents);
 		view.appendChild(div);
 	}.bind(this);
@@ -68,6 +135,32 @@ function DataNode(gambeziNode, parentDiv) {
 	if(parentDiv != null) {
 		parentDiv.appendChild(this.div);
 	}
+}
+
+function clear_contents(gambeziNode, div, contents) {
+	// Remove all children
+	while(contents.firstChild) contents.removeChild(contents.firstChild);
+	clearTimeout(div.getAttribute('timer_ident'));
+}
+
+function create_input_number(gambeziNode, div, contents) {
+	let field = document.createElement('input');
+	field.onchange = function(event) {
+		gambeziNode.set_double(field.value);
+	};
+	contents.appendChild(field);
+	div.setAttribute('data_type', 'input_number');
+}
+
+function create_output_number(gambeziNode, div, contents) {
+	let field = document.createElement('input');
+	field.readOnly = true;
+	let ident = setInterval(function() {
+		field.value = gambeziNode.get_double();
+	}, 100);
+	contents.appendChild(field);
+	div.setAttribute('data_type', 'output_number');
+	div.setAttribute('timer_ident', ident);
 }
 
 DataNode.prototype.buildTree = function() {
@@ -89,7 +182,7 @@ DataNode.prototype.buildTree = function() {
 ////////////////////////////////////////////////////////////////////////////////
 // Create draggable interface
 var grid_size = 50;
-interact('#view>div')
+interact('.view_node')
 .draggable({
 	snap: {
 		range: Infinity,
